3bbe1901ea8994d17c0fd601754c323be3229061
tensor-tang <tensor.tang.1223@gmail.com>
2025-12-10 08:45:07 +0000

    revert pkill (merge request !66)
    Squash merge branch 'fix' into 'main'

--- scripts/ci_comfy_ksana_tests.sh
+++ scripts/ci_comfy_ksana_tests.sh
@@ -33,7 +33,6 @@ unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
 
 ./scripts/install_dev.sh
 cd comfyui/tests
-pkill -f raylet
+# pkill -f raylet
 python workflow_test.py --workflows-file ./test_configs.json --gpus 0 --seed 321
-
 python workflow_test.py --workflows-file ./test_configs.json --gpus 0,1 --seed 321
