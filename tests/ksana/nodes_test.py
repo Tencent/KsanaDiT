811ef1434b72777ecf8be0c90dc1331cd948f69f
tensor-tang <tensor.tang.1223@gmail.com>
2026-01-08 11:57:53 +0000

    添加更多本地节点测试 (merge request !126)
    Squash merge branch 'fixengine' into 'main'

--- tests/ksana/nodes_test.py
+++ tests/ksana/nodes_test.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from dataclasses import dataclass
 
 import torch
 
@@ -12,9 +13,91 @@ from ksana.operations import KsanaLinearBackend
 COMFY_MODEL_ROOT = "/data/stable-diffusion-webui/models/diffusion_models"
 SEED = 321
 RUN_DTYPE = torch.float16
+TEST_EPS_PLACE = 7
 
 TARGET_T2V_IMG_SHAPE = [1, 16, 16, 32, 32]
 TARGET_I2V_IMG_SHAPE = [1, 20, 16, 32, 32]
+
+
+@dataclass
+class KsanaNodesTestCase:
+    model_names: list[str]
+    image_latent_shape: list[int]
+    attention_backends: KsanaAttentionBackend
+    linear_backends: KsanaLinearBackend
+    rope_function: str
+    expect_model_keys: list[KsanaModelKey]
+    expect_generator_outputs: list[float]
+
+
+test_cases = [
+    KsanaNodesTestCase(
+        model_names=[
+            "wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors",
+            "wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors",
+        ],
+        image_latent_shape=[TARGET_T2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.SAGE_ATTN,
+        linear_backends=KsanaLinearBackend.DEFAULT,
+        rope_function="comfy",
+        expect_model_keys=[KsanaModelKey.Wan2_2_T2V_14B_HIGH, KsanaModelKey.Wan2_2_T2V_14B_LOW],
+        expect_generator_outputs=0.78076171875,
+    ),
+    KsanaNodesTestCase(
+        model_names=[
+            "wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors",
+            "wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors",
+        ],
+        image_latent_shape=[TARGET_I2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.SAGE_ATTN,
+        linear_backends=KsanaLinearBackend.DEFAULT,
+        rope_function="default",
+        expect_model_keys=[KsanaModelKey.Wan2_2_I2V_14B_HIGH, KsanaModelKey.Wan2_2_I2V_14B_LOW],
+        expect_generator_outputs=0.78076171875,
+    ),
+    KsanaNodesTestCase(
+        model_names=["wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors", None],
+        expect_model_keys=[KsanaModelKey.Wan2_2_T2V_14B_HIGH],
+        image_latent_shape=[TARGET_T2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.FLASH_ATTN,
+        linear_backends=KsanaLinearBackend.FP8_GEMM,
+        rope_function="comfy",
+        expect_generator_outputs=0.78173828125,
+    ),
+    KsanaNodesTestCase(
+        model_names=[
+            "wan2.2_t2v_high_noise_14B_fp16.safetensors",
+            "wan2.2_t2v_low_noise_14B_fp16.safetensors",
+        ],
+        expect_model_keys=[KsanaModelKey.Wan2_2_T2V_14B_HIGH, KsanaModelKey.Wan2_2_T2V_14B_LOW],
+        image_latent_shape=[TARGET_T2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.SAGE_ATTN,
+        linear_backends=KsanaLinearBackend.FP8_GEMM_DYNAMIC,
+        rope_function="default",
+        expect_generator_outputs=0.77880859375,
+    ),
+    KsanaNodesTestCase(
+        model_names=[
+            "wan2.2_i2v_high_noise_14B_fp16.safetensors",
+            "wan2.2_i2v_low_noise_14B_fp16.safetensors",
+        ],
+        expect_model_keys=[KsanaModelKey.Wan2_2_I2V_14B_HIGH, KsanaModelKey.Wan2_2_I2V_14B_LOW],
+        image_latent_shape=[TARGET_I2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.SAGE_ATTN,
+        linear_backends=KsanaLinearBackend.FP8_GEMM_DYNAMIC,
+        rope_function="default",
+        expect_generator_outputs=0.77880859375,
+    ),
+    KsanaNodesTestCase(
+        model_names=["wan2.2_i2v_high_noise_14B_fp16.safetensors", None],
+        expect_model_keys=[KsanaModelKey.Wan2_2_I2V_14B_HIGH],
+        image_latent_shape=[TARGET_I2V_IMG_SHAPE],
+        attention_backends=KsanaAttentionBackend.FLASH_ATTN,
+        linear_backends=KsanaLinearBackend.FP16_GEMM,
+        rope_function="comfy",
+        expect_generator_outputs=0.78076171875,
+    ),
+]
 
 
 # TODO:(TJ): nodes should have both single and gpus
@@ -24,57 +107,6 @@ class TestNodes(unittest.TestCase):
         # TODO(TJ): add engine gpus
         ksana_engine = get_engine()
         ksana_engine.clear_models()
-
-        model_names = [
-            ["wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors", "wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"],
-            ["wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors", "wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"],
-            ["wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors", None],
-            ["wan2.2_t2v_high_noise_14B_fp16.safetensors", "wan2.2_t2v_low_noise_14B_fp16.safetensors"],
-            # ["wan2.2_i2v_high_noise_14B_fp16.safetensors", "wan2.2_i2v_low_noise_14B_fp16.safetensors"],
-            # ["wan2.2_i2v_high_noise_14B_fp16.safetensors", None],
-        ]
-        image_latent_shapes = [
-            TARGET_T2V_IMG_SHAPE,
-            TARGET_I2V_IMG_SHAPE,
-            TARGET_T2V_IMG_SHAPE,
-            TARGET_T2V_IMG_SHAPE,
-            # TARGET_I2V_IMG_SHAPE,
-            # TARGET_I2V_IMG_SHAPE,
-        ]
-        expect_model_keys = [
-            [KsanaModelKey.Wan2_2_T2V_14B_HIGH, KsanaModelKey.Wan2_2_T2V_14B_LOW],
-            [KsanaModelKey.Wan2_2_I2V_14B_HIGH, KsanaModelKey.Wan2_2_I2V_14B_LOW],
-            [KsanaModelKey.Wan2_2_T2V_14B_HIGH],
-            [KsanaModelKey.Wan2_2_T2V_14B_HIGH, KsanaModelKey.Wan2_2_T2V_14B_LOW],
-            # [KsanaModelKey.Wan2_2_I2V_14B_HIGH, KsanaModelKey.Wan2_2_I2V_14B_LOW],
-            # [KsanaModelKey.Wan2_2_I2V_14B_HIGH],
-        ]
-
-        attention_backends = [
-            KsanaAttentionBackend.SAGE_ATTN,
-            KsanaAttentionBackend.SAGE_ATTN,
-            KsanaAttentionBackend.FLASH_ATTN,
-            KsanaAttentionBackend.SAGE_ATTN,
-            # KsanaAttentionBackend.FLASH_ATTN,
-            # KsanaAttentionBackend.SAGE_ATTN,
-        ]
-        linear_backends = [
-            KsanaLinearBackend.DEFAULT,
-            KsanaLinearBackend.FP8_GEMM_DYNAMIC,
-            KsanaLinearBackend.FP8_GEMM,
-            KsanaLinearBackend.DEFAULT,
-            # KsanaLinearBackend.FP8_GEMM_DYNAMIC,
-            # KsanaLinearBackend.DEFAULT,
-        ]
-
-        rope_functions = [
-            "comfy",
-            "default",
-            "comfy",
-            "default",
-            # "default",
-            # "comfy",
-        ]
 
         seed_g = torch.Generator(device="cuda")
         seed_g.manual_seed(SEED)
@@ -92,25 +124,25 @@ class TestNodes(unittest.TestCase):
             generator=seed_g,
         )
 
-        for model_name, expect_key, image_latent_shape, attention_backend, linear_backend, rope_function in zip(
-            model_names, expect_model_keys, image_latent_shapes, attention_backends, linear_backends, rope_functions
-        ):
-            print(f"----------- test model_name: {model_name} -------------")
-            high_noise_model_path = os.path.join(COMFY_MODEL_ROOT, model_name[0])
-            low_noise_model_path = os.path.join(COMFY_MODEL_ROOT, model_name[1]) if model_name[1] else None
+        for test_case in test_cases:
+            print(f"----------- test model_name: {test_case.model_names} -------------")
+            high_noise_model_path = os.path.join(COMFY_MODEL_ROOT, test_case.model_names[0])
+            low_noise_model_path = (
+                os.path.join(COMFY_MODEL_ROOT, test_case.model_names[1]) if test_case.model_names[1] else None
+            )
             output = nodes.KsanaNodeModelLoader.load(
                 high_noise_model_path=high_noise_model_path,
                 low_noise_model_path=low_noise_model_path,
                 attention_config=KsanaAttentionConfig(
-                    backend=attention_backend,
+                    backend=test_case.attention_backends,
                 ),
-                linear_backend=linear_backend,
+                linear_backend=test_case.linear_backends,
                 model_boundary=0.5,
             )
-            self.assertEqual(output.model, expect_key)
+            self.assertEqual(output.model, test_case.expect_model_keys)
 
             image_latent = torch.zeros(
-                *image_latent_shape,
+                *test_case.image_latent_shape,
                 dtype=RUN_DTYPE,
                 device="cuda",
             )
@@ -122,18 +154,20 @@ class TestNodes(unittest.TestCase):
                 latent_image=nodes.KsanaNodeVAEEncodeOutput(samples=image_latent),
                 steps=1,
                 seed=SEED,
-                rope_function=rope_function,
+                rope_function=test_case.rope_function,
                 low_sample_guide_scale=3.0,
             )
             generate_output = generate_output.samples
             print(f"----------- generate_output.shape: {generate_output.shape}")
-            with self.subTest(msg="bs1 Shape Check"):
-                target_latent_shape = image_latent_shape
+            with self.subTest(msg="generate Shape Check"):
+                target_latent_shape = test_case.image_latent_shape
                 target_latent_shape[1] = 16  # always 16
                 self.assertEqual(list(generate_output.shape), target_latent_shape)
 
             mean = generate_output.cpu().abs().mean().item()
             print(f"----------- mean: {mean}")
+            # with self.subTest(msg="generate output Mean Check"):
+            # self.assertAlmostEqual(mean, test_case.expect_generator_outputs, places=TEST_EPS_PLACE)
 
 
 if __name__ == "__main__":
